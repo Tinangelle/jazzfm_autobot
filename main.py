889457929b8a8a8c25dd95a8ab47dbfc8286b656
33 <angelxql0206@gmail.com>
2025-07-17 23:52:03 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import discord
 import asyncio
 import os
+import requests
 
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 TARGET_USER_ID = int(os.getenv("TARGET_USER_ID"))
 TARGET_VOICE_CHANNEL_ID = int(os.getenv("TARGET_VOICE_CHANNEL_ID"))
-TARGET_TEXT_CHANNEL_ID = int(os.getenv("TARGET_TEXT_CHANNEL_ID"))
+
+WEBHOOK_URL = "https://discord.com/api/webhooks/1395432088139730944/S-XiYoNIcpz9d0ywEyvQPkYAYJxhXdBP6ua_mNA_4AehhSlX6bxYrxaWkmmBHOPZ2Df8"
 
 intents = discord.Intents.default()
 intents.voice_states = True
 intents.guilds = True
 intents.members = True
-intents.messages = True
-intents.message_content = True
 
 client = discord.Client(intents=intents)
 
@@ -24,11 +24,12 @@ async def on_ready():
 async def on_voice_state_update(member, before, after):
     if member.id == TARGET_USER_ID:
         if after.channel and after.channel.id == TARGET_VOICE_CHANNEL_ID:
-            text_channel = client.get_channel(TARGET_TEXT_CHANNEL_ID)
-            if text_channel:
-                await text_channel.send("m!join")
-                await asyncio.sleep(1.5)
-                await text_channel.send("m!play http://jazzfm91.streamb.live/SB00009")
-                print("已发送 join 和播放命令。")
+            print("检测到目标用户进入语音频道，发送 Webhook 指令")
+            data = {
+                "content": "m!play http://jazzfm91.streamb.live/SB00009",
+                "username": "fake33"
+            }
+            response = requests.post(WEBHOOK_URL, json=data)
+            print("Webhook 响应状态码：", response.status_code)
 
 client.run(TOKEN)
